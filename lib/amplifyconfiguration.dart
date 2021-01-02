98643ec99e336a86c74eed665284643f0f1bbf29
nurnadiahbtruslan <nurnadiahbtruslan@gmail.com>
2021-01-02 22:53:09 +0800

    commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:7512d39b-cd8d-49d4-a989-cb9f4be8ba67",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_xYQSnwlGl",
+                        "AppClientId": "3n4c8pkp2ucjttfsuccs4vlgh8",
+                        "AppClientSecret": "fp5gqkblgt3mnqidrrlceqg2j42l059kvhod5330aqkm4v4abhf",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
